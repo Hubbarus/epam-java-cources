06593b2e72c3e2a813b25d1234fea6d336939a78
Paul Ponomarev <rocksquare@gmail.com>
2020-10-04 17:33:05 +0300

    task041 done

---
+++ src/main/java/com/epam/university/java/core/task041/EntityImpl.java
@@ -0,0 +1,39 @@
+package com.epam.university.java.core.task041;
+
+import java.util.Objects;
+
+public class EntityImpl implements Entity {
+    private String value;
+    private int id;
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EntityImpl)) return false;
+        EntityImpl entity = (EntityImpl) o;
+        return Objects.equals(getValue(), entity.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue(), getId());
+    }
+}
