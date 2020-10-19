e10a23fe66e57d5aa4162cefec654aabc46c36eb
Paul Ponomarev <rocksquare@gmail.com>
2020-10-19 12:43:20 +0300

    app context implemented, tests passed

---
+++ src/main/java/com/epam/university/java/project/core/cdi/bean/BeanFactoryImpl.java
@@ -0,0 +1,95 @@
+package com.epam.university.java.project.core.cdi.bean;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class BeanFactoryImpl implements BeanFactory {
+    private BeanDefinitionRegistry registry;
+    private BeanSetterHelper setter;
+    private HashMap<Class<?>, Object> singletonMap = new HashMap<>();
+
+    public BeanFactoryImpl(BeanDefinitionRegistry registry) {
+        this.registry = registry;
+        this.setter = new BeanSetterHelper(this);
+    }
+
+    @Override
+    public <T> T getBean(Class<T> beanClass) {
+        System.out.println();
+        BeanDefinitionRegistryImpl reg = (BeanDefinitionRegistryImpl) registry;
+        Collection<BeanDefinition> beans = reg.getCollection().getBeans();
+        for (BeanDefinition bean : beans) {
+            Class<?> clazz = null;
+            try {
+                 clazz = Class.forName(bean.getClassName());
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            Class<?>[] interfaces = clazz.getInterfaces();
+            if (clazz.equals(beanClass) || Arrays.asList(interfaces).contains(beanClass)) {
+                return (T) getBean(bean.getId());
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Object getBean(String beanName) {
+        BeanDefinition beanDefinition = registry.getBeanDefinition(beanName);
+
+        Class<?> beanClass = null;
+        try {
+            beanClass = Class.forName(beanDefinition.getClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (singletonMap.containsKey(beanClass)) {
+            return singletonMap.get(beanClass);
+        }
+
+        Object bean = null;
+
+        try {
+            bean = Objects.requireNonNull(beanClass).getConstructor().newInstance();
+            Collection<BeanPropertyDefinition> properties = beanDefinition.getProperties();
+            if (properties != null && properties.size() > 0) {
+                for (BeanPropertyDefinition property : properties) {
+                    Field field = beanClass.getDeclaredField(property.getName());
+                    if (property.getRef() != null) {
+                        setter.setReference(field, bean, property);
+                    } else if (property.getData() == null) {
+                        setter.setValue(field, bean, property);
+                    } else if (property.getData() != null) {
+                        setter.setData(field, bean, property);
+                    }
+                }
+            }
+
+            if (beanDefinition.getPostConstruct() != null) {
+                beanClass.getMethod(beanDefinition.getPostConstruct()).invoke(bean);
+            }
+
+            if (beanDefinition.getScope() != null
+                    && beanDefinition.getScope().equals("singleton")) {
+                singletonMap.put(beanClass, bean);
+            }
+        } catch (InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException
+                | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        return bean;
+    }
+
+    @Override
+    public <T> T getBean(String beanName, Class<T> beanClass) {
+        return (T) getBean(beanName);
+    }
+}
---
+++ src/main/java/com/epam/university/java/project/core/cdi/bean/BeanSetterHelper.java
@@ -0,0 +1,85 @@
+package com.epam.university.java.project.core.cdi.bean;
+
+import com.epam.university.java.project.core.cdi.structure.ListDefinition;
+import com.epam.university.java.project.core.cdi.structure.MapDefinition;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BeanSetterHelper {
+    private BeanFactoryImpl factory;
+
+    public BeanSetterHelper(BeanFactoryImpl factory) {
+        this.factory = factory;
+    }
+
+    public void setValue(Field field, Object bean, BeanPropertyDefinition property) throws IllegalAccessException {
+        Class<?> type = field.getType();
+        Object val = null;
+        if (type.equals(int.class)) {
+            val = Integer.parseInt(property.getValue());
+        } else {
+            val = property.getValue();
+        }
+
+        if (val == null) {
+            throw new RuntimeException();
+        }
+
+        field.setAccessible(true);
+        field.set(bean, val);
+    }
+
+    public void setReference(Field field, Object bean, BeanPropertyDefinition property) throws IllegalAccessException {
+        Object val = null;
+        Class<?> type = field.getType();
+        String ref = property.getRef();
+        if (ref != null) {
+            val = factory.getBean(ref);
+            field.setAccessible(true);
+            field.set(bean, val);
+        }
+    }
+
+    public void setData(Field field, Object bean, BeanPropertyDefinition property) throws IllegalAccessException {
+        switch (field.getName()) {
+            case "stringCollection" : {
+                Collection<String> data = new ArrayList<>();
+                ListDefinition definition = (ListDefinition) property.getData();
+                Collection<ListDefinition.ListItemDefinition> items = definition.getItems();
+                for (ListDefinition.ListItemDefinition item : items) {
+                    data.add(item.getValue());
+                }
+
+                field.setAccessible(true);
+                field.set(bean, data);
+            } break;
+            case "stringMap" : {
+                Map<String, String> map = new HashMap<>();
+                MapDefinition definition = (MapDefinition) property.getData();
+                Collection<MapDefinition.MapEntryDefinition> values = definition.getValues();
+                for (MapDefinition.MapEntryDefinition entry : values) {
+                    map.put(entry.getKey(), entry.getValue());
+                }
+
+                field.setAccessible(true);
+                field.set(bean, map);
+            } break;
+
+            case "objectMap" : {
+                Map<String, Object> map = new HashMap<>();
+                MapDefinition definition = (MapDefinition) property.getData();
+                Collection<MapDefinition.MapEntryDefinition> values = definition.getValues();
+                for (MapDefinition.MapEntryDefinition entry : values) {
+                    map.put(entry.getKey(), factory.getBean(entry.getRef()));
+                }
+
+                field.setAccessible(true);
+                field.set(bean, map);
+            } break;
+        }
+    }
+}
