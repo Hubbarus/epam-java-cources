a8b314b8fcf81e5a51e2fca2d00e4996b9a35d1b
Paul Ponomarev <rocksquare@gmail.com>
2020-11-10 14:50:41 +0300

    project done!

--- src/main/java/com/epam/university/java/project/core/state/machine/domain/HandlerXmlAdapter.java
+++ src/main/java/com/epam/university/java/project/core/state/machine/domain/HandlerXmlAdapter.java
@@ -2,14 +2,14 @@ package com.epam.university.java.project.core.state.machine.domain;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-public class HandlerClassAdapter extends XmlAdapter<String, Class<?>> {
+public class HandlerXmlAdapter extends XmlAdapter<String, Class<?>> {
 
     @Override
     public Class<?> unmarshal(String v) throws Exception {
         return Class.forName(v);
     }
 
-    
+
     @Override
     public String marshal(Class<?> v) throws Exception {
         return v.getName();
--- src/main/java/com/epam/university/java/project/domain/BookImpl.java
+++ src/main/java/com/epam/university/java/project/domain/BookImpl.java
@@ -1,4 +1,110 @@
 package com.epam.university.java.project.domain;
 
-public class BookImpl {
+import com.epam.university.java.project.core.state.machine.domain.StateMachineDefinition;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Objects;
+
+public class BookImpl implements Book {
+    private int id;
+    private String title;
+    private Collection<String> authors;
+    private String serialNumber;
+    private LocalDate returnDate;
+    private BookStatus status;
+    private StateMachineDefinition<BookStatus, BookEvent> definition;
+
+    public BookImpl(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public Collection<String> getAuthors() {
+        return authors;
+    }
+
+    @Override
+    public void setAuthors(Collection<String> authors) {
+        this.authors = authors;
+    }
+
+    @Override
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    @Override
+    public void setSerialNumber(String value) {
+        this.serialNumber = value;
+    }
+
+    @Override
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    @Override
+    public void setReturnDate(LocalDate date) {
+        this.returnDate = date;
+    }
+
+    @Override
+    public BookStatus getState() {
+        return status;
+    }
+
+    @Override
+    public void setState(BookStatus bookStatus) {
+        this.status = bookStatus;
+    }
+
+    @Override
+    public StateMachineDefinition<BookStatus, BookEvent> getStateMachineDefinition() {
+        return definition;
+    }
+
+    @Override
+    public void setStateMachineDefinition(StateMachineDefinition<BookStatus, BookEvent> definition) {
+        this.definition = definition;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BookImpl)) return false;
+        BookImpl book = (BookImpl) o;
+        return getId() == book.getId() &&
+                Objects.equals(getTitle(), book.getTitle()) &&
+                Objects.equals(getAuthors(), book.getAuthors()) &&
+                Objects.equals(getSerialNumber(), book.getSerialNumber()) &&
+                Objects.equals(getReturnDate(), book.getReturnDate()) &&
+                status == book.status &&
+                Objects.equals(definition, book.definition);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle(), getAuthors(), getSerialNumber(), getReturnDate(), status, definition);
+    }
 }
